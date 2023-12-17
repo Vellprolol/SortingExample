cd7b9c4b2d34c93382dcc5ee3dc9670c3e138779
al.kuznetsov <al.kuznetsov@astondevs.ru>
2023-12-17 15:08:21 +0300

    Initial commit

---
+++ src/main/java/ru/alexeykuznetsov/spbgti/psd/sortingexample/SortingExampleApplication.java
@@ -0,0 +1,13 @@
+package ru.alexeykuznetsov.spbgti.psd.sortingexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortingExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SortingExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alexeykuznetsov/spbgti/psd/sortingexample/SortingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alexeykuznetsov.spbgti.psd.sortingexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortingExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
